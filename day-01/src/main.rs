154d3ac9cfa2dd1781587244932d545255892c6f
niblit <niblit.gh@gmail.com>
2023-12-02 22:07:04 -0600

    Solve part two

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let data = include_str!("../input.txt");
-    let result = solve(data);
-    println!("{result}");
+
+    let solution_one = part_one(data);
+    println!("{solution_one}");
+
+    let solution_two = part_two(data);
+    println!("{solution_two}")
 }
 
-fn solve(data: &str) -> u32 {
+fn part_one(data: &str) -> u32 {
     data.lines()
         .map(|line| {
             let digits = line
@@ -16,14 +20,42 @@ fn solve(data: &str) -> u32 {
         .sum()
 }
 
+fn part_two(data: &str) -> u32 {
+    const CONVERT: [(&str, &str); 9] = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    let mut parsed_data = data.to_owned();
+    for (key, value) in CONVERT {
+        parsed_data = parsed_data.replace(key, format!("{key}{value}{key}").as_str());
+    }
+
+    part_one(&parsed_data)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn example_one() {
         let data = include_str!("../example01.txt");
-        let result = solve(data);
+        let result = part_one(data);
         assert_eq!(142, result);
+    }
+
+    #[test]
+    fn example_two() {
+        let data = include_str!("../example02.txt");
+        let result = part_two(data);
+        assert_eq!(281, result);
     }
 }
