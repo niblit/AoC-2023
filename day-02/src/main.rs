ba6460a59406fae44202babf364c103999cd13b4
niblit <niblit.gh@gmail.com>
2023-12-13 01:07:16 -0600

    Modeled possible solution

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let result_one = part_one(input);
+
+    println!("{result_one}");
+}
+
+struct Game {
+    id: i32,
+    runs: Vec<Run>,
+}
+
+impl Game {
+    pub fn is_possible(&self, red: i32, green: i32, blue: i32) -> bool {
+        self.runs
+            .iter()
+            .all(|run| run.is_possible(red, green, blue))
+    }
+}
+
+struct Run {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl Run {
+    pub fn is_possible(&self, red: i32, green: i32, blue: i32) -> bool {
+        self.red <= red && self.green <= green && self.blue <= blue
+    }
+}
+
+fn part_one(_data: &str) -> i32 {
+    return 0;
 }
 
 #[cfg(test)]
